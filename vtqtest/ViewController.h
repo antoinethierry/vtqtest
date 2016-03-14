2cb5826e3b3f126a605f8c157700bad10fcc948a
Antoine THIERRY <antoine.thierry@vetoquinol.com>
2016-03-14 14:09:04 +0100

    Initial Commit

---
+++ vtqtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  vtqtest
+//
+//  Created by IpadVTQ on 14/03/2016.
+//  Copyright © 2016 Vetoquinol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ vtqtest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  vtqtest
+//
+//  Created by IpadVTQ on 14/03/2016.
+//  Copyright © 2016 Vetoquinol. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
